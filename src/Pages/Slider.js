7924fc106601cb555fb104459545af1ef5d499f0
O M Ganesh <ukgane@gmail.com>
2022-11-13 14:25:34 +0530

    Slider modified and Images added

--- src/Pages/Slider.js
+++ src/Pages/Slider.js
@@ -2,19 +2,23 @@ import React from 'react';
 import OwlCarousel from 'react-owl-carousel';  
 import 'owl.carousel/dist/assets/owl.carousel.css';  
 import 'owl.carousel/dist/assets/owl.theme.default.css';  
- 
+import Pic from '../BannerImages/img-1.png';
+import Pic2 from '../BannerImages/img-2.jpeg';
+import Pic3 from '../BannerImages/img-3.png'; 
+import Pic4 from '../BannerImages/img-4.png'; 
 function Slider() {
   return (
     <div>
-       <div className='container-fluid' >            
+       <div className='container-fluid p-0' >            
         <OwlCarousel items={1}  autoplay ={true}
           className="owl-theme"  
           loop  
           nav  
           margin={8} >  
-           <div ><img  className="img" src= {'assets/img/img1.jpg'} alt="" /></div>  
-           <div><img  className="img" src= {'assets/img/img2.jpg'} alt="" /></div>  
-           <div><img  className="img" src= {'assets/img/img4.jpg'} alt="" /></div>   
+          <div><img  className="img" src= {Pic} alt="" /></div>
+           <div><img  className="img" src= {Pic2} alt="" /></div>  
+           <div><img  className="img" src= {Pic3} alt="" /></div>  
+           <div><img  className="img" src= {Pic4} alt="" /></div>   
       </OwlCarousel>  
       </div>  
     </div>
